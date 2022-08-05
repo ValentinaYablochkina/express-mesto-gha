425c82ac7c005c260f47162520882022e8ddae72
Valentina Yablochkina <tino4ka9215@mail.ru>
2022-08-05 20:31:08 +0300

    ПР14

--- routes/users.js
+++ routes/users.js
@@ -41,9 +41,6 @@ router.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(regex),
   }),
 }), createUser);
 module.exports = router;
